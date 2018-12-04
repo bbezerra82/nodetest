fb2e65e01a1ee10b08314d0ee4c023eff4fdf3d4
Bernardo <b.alvim@gmail.com>
2018-12-04 09:10:05 +0000

    latest tests, figuring out jsonquery

--- jw/jw.js
+++ jw/jw.js
@@ -1,49 +1,211 @@
-const JustWatch = require('justwatch-api');
+const JustWatch = require('./justWatchAPI.js');
 const JSONQuery = require('json-query');
+const jq = require('node-jq');
 
 function printResult(name, result) {
-    console.log(name + ': ');
+    // console.log(name + ': ');
     console.log(JSON.stringify(result, null, 4));
     // console.log('\n\n\n\n\n');
 }
 
 (async function () {
     let jw = new JustWatch({
-        locale: 'en_US'
+        locale: 'en_GB'
     });
+    // printResult('', searchResult);
 
-    let searchResult = await jw.search({
-        query: 'the ring'
-    });
-    // printResult('search', searchResult);
+    // let title = await jw.getTitle(top5[0].contentType, top5[0].id)
 
-    let providerId = searchResult.items[0].offers[0].provider_id;
+    // printResult('', title);
+
+    // let providerId = searchResult.items[0].offers[0].provider_id;
     // console.log('provider id:' + provider);
 
-    let providers = await jw.getProviders({});
+    let searchResult = await jw.search({
+        query: 'the ring',
+        page_size: 5
+        // providers: 'Netflix'
+    });
 
-    let providerName = JSONQuery(`[id=${providerId}].clear_name`, {
-        data: providers
-    }).value;
+    let arraySize = JSONQuery('.page_size', {data:searchResult}).value;
+    let title = JSONQuery('[items].title', {data:searchResult}).value;
+    let release = JSONQuery('[items].original_release_year', {data:searchResult}).value;
+    let short_description = JSONQuery('[items].short_description', {data:searchResult}).value;
 
-    console.log(`providerName = ${providerName}`);
+    // console.log(JSON.stringify(title, null, 4));
+    // console.log(JSON.stringify(release, null, 4));
+    // console.log(JSON.stringify(short_description, null, 4));
+    // console.log(JSON.stringify(offers, null, 4));
+    console.log(offers.length)
 
-    let data = [
-        {
-            "id": 8, 
-            "profile_id": 13, 
-            "technical_name": "netflix", 
-            "short_name": "nfx", 
-            "clear_name": "Netflix",
+    let movies = []
+    for (i = 0; (i < arraySize); i++) {
+        let offers = JSONQuery(`[items].offers`, {data:searchResult}).value;
+        movies.push({
+            title: title[i],
+            release: release[i],
+            description: short_description[i],
+            offers: offers[i]
+        })
+    }
+
+    // console.log(movies)
+    
+    
+    // console.log(JSON.stringify(offers, null, 4))
+     var provider = {
+        "providerA": {
+            "heardAs": "Netflix",
+            "resolved": "Netflix",
+            "ERstatus": "ER_SUCCESS_MATCH"
         },
-        {
-            "id":192,
-            "profile_id":590,
-            "technical_name":"youtube",
-            "short_name":"yot",
-            "clear_name":"YouTube"
+        "providerB": {
+            "heardAs": "BBC",
+            "resolved": "BBC iPlayer",
+            "ERstatus": "ER_SUCCESS_MATCH"
+        },
+        "providerC": {
+            "heardAs": "",
+            "resolved": "",
+            "ERstatus": ""
         }
-    ]
+    }
+
+    // let providers = []
+    // var result = JSONQuery('[*][resolved]', {data: provider}).value
+
+    // for (i = 0; (i < result.length) && (result[i] !== ''); i++) {
+
+    //     providers.push({
+    //         "id": i,
+    //         "name" : result[i]
+    //     });
+    // }
+    // console.log(providers)
+
+
+
+
+    // // var result = JSONQuery('[**][*country=NZ]', {data: data}).value
+    // var result = JSONQuery('[*][resolved]', {data: provider}).value
+
+    // for (i = 0; (i < result.length) && (result[i] !== ''); i++) {
+    //     console.log(result[i])
+    // // }
+
+    // let favourites = ['a', 'b', 'c'];
+
+    // let check = ['b', 'd', 'e', 'a'];
+
+    // for (i = 0; i < favourites.length; i++) {
+    //     if(check.includes(favourites[i])) {
+    //         check.splice(check.indexOf(favourites[i]), 1)
+    //     }
+    //     // console.log(`check inclueds ${favourites[i]}? ${check.includes(favourites[i])}`)
+    // }
+    // console.log(check)
+
+    // let data = await jw.getProviders({});
+
+    // console.log(JSON.stringify(data, null, 4));
+
+
+    // let providersList = JSONQuery('[id]', {
+    //     data: data
+    //     // data: await jw.getProviders({})
+    //   }).value;
+
+    //   console.log(providersList)
+
+    // console.log(JSON.stringify(searchResult, null, 4))
+    
+    
+    // const filterMovie = '[.items | .[] | {title, release: .original_release_year, description: .short_description, offers: [(.offers | .[] | select(.monetization_type | contains("rent")))? | {monetization_type, provider_id, presentation_type, retail_price, currency}]}]';
+    // const options = {
+    //     input: 'json'
+    // }
+
+    // let listMovie = await jq.run(filterMovie, searchResult, options)
+    // // console.log(list);
+
+
+    // let providers = await jw.getProviders({});
+    // // console.log(providers);
+    
+    // const filterProvider = '[.[] | {id, clear_name, short_name}]';
+
+    // let listProvider = await jq.run(filterProvider, providers, options);
+    // console.log(listProvider);
+
+    // try {
+    //     let providersName = await jq.run(filter2, providersList, {input: 'json'})
+    //     console.log(providersName)
+    // } catch (error) {
+    //     console.error(error)
+    // }
+
+    // let data = [
+    //     {
+    //         "id": 8, 
+    //         "profile_id": 13, 
+    //         "technical_name": "netflix", 
+    //         "short_name": "nfx", 
+    //         "clear_name": "Netflix",
+    //     },
+    //     {
+    //         "id":192,
+    //         "profile_id":590,
+    //         "technical_name":"youtube",
+    //         "short_name":"yot",
+    //         "clear_name":"YouTube"
+    //     }
+    // ]
+
+    // let providerName = JSONQuery(`[clear_name][short_name]`, {
+    //     data: data
+    //     // data: await jw.getProviders({})
+    // }).value;
+
+    // console.log(providerName)
+
+    // var data = {
+    //     grouped_people: {
+    //       'friends': [
+    //         {name: 'Steve', country: 'NZ'},
+    //         {name: 'Jane', country: 'US'},
+    //         {name: 'Mike', country: 'AU'},
+    //         {name: 'Mary', country: 'NZ'},
+    //       ],
+    //       'enemies': [
+    //         {name: 'Evil Steve', country: 'AU'},
+    //         {name: 'Betty', country: 'NZ'},
+    //       ]
+    //     }
+    //   }
+    
+    //     var result = JSONQuery('grouped_people[**][*country=NZ]', {data: data}).value
+
+    //     console.log(result)
+
+
+
+    // var data = {
+    //     people: [
+    //       {name: 'Matt', country: 'NZ'},
+    //       {name: 'Pete', country: 'AU'},
+    //       {name: 'Mikey', country: 'NZ'}
+    //     ]
+    //   }
+       
+    //   let providerName = JSONQuery('people[country=NZ].name', {
+    //     data: data
+    //   }).value
+
+    // for (i = 0; i < providerName.length; i++) {
+    //     console.log(providerName[i]);
+    // }
+    // console.log(`providerName = ${providerName}`);
+    // printResult('', providerName);
     // var providerName = JSONQuery('[id=192].clear_name', {
     //     data: data
     // })
@@ -65,5 +227,18 @@ function printResult(name, result) {
     // printResult('title', title);
 
 
+
+
+    // let favourites = ['a', 'b', 'c'];
+
+    // let check = ['b', 'd', 'e', 'a'];
+
+    // for (i = 0; i < favourites.length; i++) {
+    //     if(check.includes(favourites[i])) {
+    //         check.splice(check.indexOf(favourites[i]), 1)
+    //     }
+    //     // console.log(`check inclueds ${favourites[i]}? ${check.includes(favourites[i])}`)
+    // }
+    // console.log(check)
 
 })();
